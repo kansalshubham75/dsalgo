c4b20a4a5f95d54c4ddbd2428773fd002dc50abd
Shubham Kansal <kansalshubham75@gmail.com>
2020-09-16 16:20:32 +0530

    update

--- test.cpp
+++ test.cpp
@@ -1,83 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
-int cnttrue(string A) {
-    vector<int> operands;
-    vector<char> op;
-    for(char c:A){
-        if(c=='T'){
-            operands.push_back(1);
-        }else if(c=='F'){
-            operands.push_back(0);
-        }else{
-            op.push_back(c);
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define pb push_back
+#define vc vector
+
+class BIT{
+    public:
+    int size;
+    vector<int> bit;
+    BIT(int n){
+        size=n+1;
+        bit.assign(size,0);
+    }
+    int sum(int i){     //get prefix sum [1.....i]
+        int ans=0;
+        while(i>0){
+            ans+=bit[i];
+            i-=(i&-i);
+        }
+        return ans;
+    }
+    void update(int i,int delta){
+        while(i<size){
+            bit[i]+=delta;
+            i+=(i&-i);
         }
     }
-    int n=operands.size();
-    int t[n+1][n+1];
-    int f[n+1][n+1];
-    memset(t,0,sizeof(t));
-    memset(f,0,sizeof(f));
-    for(int i=0;i<n;i++){
-            t[i][i]=operands[i]==1?1:0;
-            f[i][i]=operands[i]==0?1:0;
+    
+    int rangeSum(int l,int r){      //range sum[l....r]
+        return sum(r)-sum(l-1);
     }
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<n;j++)
-    //     cout<<t[i][j]<<" ";
-    //     cout<<endl;
-    // }
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<n;j++)
-    //     cout<<f[i][j]<<" ";
-    //     cout<<endl;
-    // }
-    for(int gap=1;gap<n;gap++){
-        for(int l=0;l+gap<n;l++){
-            int r=l+gap;
-                cout<<l<<" "<<r<<endl;
-                for(int cut=l;cut<r;cut++){
-                    cout<<cut;
-                    if(op[cut]=='&'){
-                        cout<<" -> & \n";
-                        t[l][r]+=(t[l][cut]*t[cut+1][r]);
-                        f[l][r]+=(t[l][cut]*f[cut+1][r])+(f[l][cut]*t[cut+1][r])+
-                                (f[l][cut]*f[cut+1][r]);
-                    }else if(op[cut]=='|'){
-                        cout<<" -> | \n";
-                        t[l][r]+=(t[l][cut]*f[cut+1][r])+(f[l][cut]*t[cut+1][r])+
-                                (t[l][cut]*t[cut+1][r]);
-                        f[l][r]+=(f[l][cut]*f[cut+1][r]);
-                    }else{
-                        cout<<" -> ^ \n";
-                        t[l][r]+=(t[l][cut]*f[cut+1][r])+(f[l][cut]*t[cut+1][r]);
-                        f[l][r]+=(f[l][cut]*f[cut+1][r])+(t[l][cut]*t[cut+1][r]);
-                    }
-                    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)
-        cout<<t[i][j]<<" ";
-        cout<<endl;
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)
-        cout<<f[i][j]<<" ";
-        cout<<endl;
-    }
-                }
-                cout<<endl;
-        }
-    }
-    // for(auto i:op)cout<<i<<" ";
-    // for(int i=0;i<n;i++){
-    //     for(int j=0;j<n;j++)
-    //         cout<<t[i][j]<<" ";
-    //     cout<<"\n";
-    // }
-    return t[0][n-1];
+};
+
+
+int bSearch(int lo,ll val,vector<ll>& arr){
+	int hi=arr.size()-1;
+	while(lo<hi){
+		int mid=lo+((hi-lo)/2);
+		if(arr[mid]<val){
+			lo=mid+1;
+		}else{
+			hi=mid;
+		}
+	}
+	return hi;
 }
-
-
 int main(){
-    cout<<cnttrue("F|T^T&F");
-    return 0;
+	int n,k;
+	cin>>n>>k;
+	vc<int> arr(n);
+	for(int &a:arr)cin>>a;
+	vector<int> original_array(n);
+    for(int i=0;i<n;i++)original_array[i]=arr[i];
+	sort(arr.begin(),arr.end());
+	unordered_map<int,int> m;
+	int i=1,val=1;
+	m[arr[0]]=1;
+	for(;i<n;i++){
+		if(arr[i]!=arr[i-1])val++;
+		m[arr[i]]=val;
+	}
+    // for(int i=0;i<n;i++)cout<<m[arr[i]]<<" ";
+    // cout<<endl;
+	BIT bit(val);
+    // for(int i=0;i<n;i++)cout<<m[arr[i]]<<" ";
+	for(i=0;i<n;i++){
+		bit.update(m[arr[i]],1);
+	}
+	vector<ll> bigger(n,0);
+	for(int i=0;i<n;i++){
+		bigger[i]=bit.rangeSum(m[original_array[i]]+1,val);
+		bit.update(m[original_array[i]],-1);
+	}
+    ll ans=0;
+	sort(bigger.begin(),bigger.end());
+    // cout<<bSearch(8,bigger);
+    // for(int i:bigger)cout<<i<<" ";
+	for(int i=0;i<n;i++){
+        // cout<<bigger[i]<<" ";
+		ll search_val=k-bigger[i];
+		int idx=bSearch(i+1,search_val,bigger);
+        if(bigger[idx]<search_val || idx==i)idx++;
+        // cout<<n-idx<<endl;
+		ans+=(n-idx);
+	}
+	cout<<ans;
+	return 0;
 }
